0bce150b477e26502af3e364bc9393925b4de417
Orinten <1595583713@qq.com>
2022-10-14 15:38:32 +0800

    注释多余的main，运营代码

--- eBook/examples/chapter_2/hello_world1.go
+++ eBook/examples/chapter_2/hello_world1.go
@@ -1,6 +1,14 @@
+/*
+ * @Author: Orinten 1595583713@qq.com
+ * @Date: 2022-10-14 15:30:20
+ * @LastEditors: Orinten 1595583713@qq.com
+ * @LastEditTime: 2022-10-14 15:34:48
+ * @FilePath: \000007_GoWorkSpace\221014_TheWayToGo\eBook\examples\chapter_2\hello_world1.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // hello_world1.go
 package main
 
-func main() {
-	println("Hello", "world")
-}
+// func main() {
+// 	println("Hello", "world")
+// }
